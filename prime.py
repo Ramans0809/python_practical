332bf4f8fe7fc386d43d90d2694d37b59acd6d04
Ramans0809 <87927765+Ramans0809@users.noreply.github.com>
2022-11-04 14:18:30 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,13 +1,5 @@
-from math import sqrt
 import sys
-# Number to be checked for prime
-n=int(sys.argv[1])
-
-if(n>1):
-   if(n%2==0):
-print(n," is a Prime Number!")
-   else:
-print(n," is a not Prime Number!")
-else:
-   print(n," is Not a Prime Number!")	
-
+x=int(sys.argv[1])
+y=int(sys.argv[2])
+sum=x+y
+print("The addition is :",sum)
