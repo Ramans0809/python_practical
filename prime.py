624d81edaff6d70b37b716b8069adc6dd8823c3b
Ramans0809 <87927765+Ramans0809@users.noreply.github.com>
2022-11-04 14:26:12 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,5 +1,19 @@
 import sys
-x=int(sys.argv[1])
-y=int(sys.argv[2])
-sum=x+y
-print("The addition is :",sum)
+from math import sqrt
+
+# Number to be checked for prime
+n =int(sys.argv[1])
+
+flag = 0
+
+if(n > 1):
+	for k in range(2, int(sqrt(n)) + 1):
+		if (n % k == 0):
+			flag = 1
+		break
+	if (flag == 0):
+		print(n," is a Prime Number!")
+	else:
+		print(n," is Not a Prime Number!")
+else:
+	print(n," is Not a Prime Number!")
