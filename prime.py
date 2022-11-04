046309689bf502d24291c0dbb979cd46747db5d4
Ramans0809 <87927765+Ramans0809@users.noreply.github.com>
2022-11-04 14:15:48 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -5,9 +5,9 @@ n=int(sys.argv[1])
 
 if(n>1):
   if(n%2==0):
-	print(n," is a Prime Number!")
+print(n," is a Prime Number!")
   else:
-	print(n," is a not Prime Number!")
+print(n," is a not Prime Number!")
 else:
-    print(n," is Not a Prime Number!")	
+   print(n," is Not a Prime Number!")	
 
