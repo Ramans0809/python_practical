fff6ba99c2239da3aa6ed50354a60cd01b97a0f3
Ramans0809 <87927765+Ramans0809@users.noreply.github.com>
2022-11-04 14:14:46 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -3,16 +3,11 @@ import sys
 # Number to be checked for prime
 n=int(sys.argv[1])
 
-flag = 0
-
-if(n > 1):
-	for k in range(2, int(sqrt(n)) + 1):
-		if (n % k == 0):
-			flag = 1
-		break
-	if (flag == 0):
-		print(n," is a Prime Number!")
-	else:
-		print(n," is Not a Prime Number!")
+if(n>1):
+  if(n%2==0):
+	print(n," is a Prime Number!")
+  else:
+	print(n," is a not Prime Number!")
 else:
-	print(n," is Not a Prime Number!")
+    print(n," is Not a Prime Number!")	
+
